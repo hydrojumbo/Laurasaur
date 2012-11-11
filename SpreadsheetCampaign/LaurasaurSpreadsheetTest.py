167352097d441d5fa45926dbfe1febe83e4c6546
Alex Bedig <alex.bedig@tufts.edu>
2012-11-10 20:01:33 -0500

    Update SpreadsheetCampaign/LaurasaurSpreadsheetTest.py

--- SpreadsheetCampaign/LaurasaurSpreadsheetTest.py
+++ SpreadsheetCampaign/LaurasaurSpreadsheetTest.py
@@ -1,7 +1,12 @@
+# dependencies of this .py module
 import LaurasaurSpreadsheetCampaign
+
+# parameters you set
 template = 'D:\Dev\Laurasaur\Laurasaur\SpreadsheetCampaign\DemoCampaignTemplate.txt'
 data = 'D:\Dev\Laurasaur\Laurasaur\SpreadsheetCampaign\DemoCampaignSpreadsheet.xls'
 sendFromGmailAddress = 'youremail@gmail.com'
 sendFromGmailUserName = 'yourGmailUserName'
 sendFromGmailPassword = 'yourGmailPassword'
+
+# do the email campaign
 LaurasaurSpreadsheetCampaign.main(template, data, sendFromGmailAddress, sendFromGmailUserName, sendFromGmailPassword)
